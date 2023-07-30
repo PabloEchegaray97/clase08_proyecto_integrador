d0988d9cc668b6d7d17f517cb658390b44715dbe
pae <pablo.echegaray97@gmail.com>
2023-07-30 04:22:08 -0300

    cart.router w status code & quantity check

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -1,29 +1,51 @@
-import { Router } from 'express'
-import CartModel from '../DAO/mongoManager/models/cart.model.js'
+import { Router } from 'express';
+import CartModel from '../DAO/mongoManager/models/cart.model.js';
 
-const router = Router()
+const router = Router();
 
 router.get('/', async (req, res) => {
-    const result = await CartModel.find()
-    res.send(result)
-})
+    try {
+        const carts = await CartModel.find();
+        res.status(200).json(carts);
+    } catch (error) {
+        console.error('Error fetching carts from the database:', error);
+        res.status(500).send('Error fetching carts from the database');
+    }
+});
 
-// Add product to cart
-router.get('/:idc/:idp', async (req, res) => {
-    const idc = req.params.idc
-    const idp = req.params.idp
-    const quantity = req.query.quantity || 1
+router.get('/:cartId/:productId', async (req, res) => {
+    try {
+        const cartId = req.params.cartId;
+        const productId = req.params.productId;
+        const quantity = req.query.quantity || 1;
 
-    const cart = await CartModel.findById(idc)
-    cart.products.push({ id: idp, quantity })
-    const result = cart.save()
+        let cart = await CartModel.findById(cartId);
+        const productIndex = cart.products.findIndex(
+            (product) => product.id === productId
+        );
+        if (productIndex !== -1) {
+            cart.products[productIndex].quantity += quantity;
+        } else {
+            cart.products.push({ id: productId, quantity });
+        }
 
-    res.send(result)
-})
+        cart = await cart.save();
+
+        res.status(200).json(cart);
+    } catch (error) {
+        console.error('Error adding product to cart:', error);
+        res.status(500).send('Error adding product to cart');
+    }
+});
 
 router.post('/', async (req, res) => {
-    const result = await CartModel.create({products: []})
-    res.send(result)
-})
+    try {
+        const cart = await CartModel.create({ products: [] });
+        res.status(201).json(cart);
+    } catch (error) {
+        console.error('Error creating new cart:', error);
+        res.status(500).send('Error creating new cart');
+    }
+});
 
-export default router
+export default router;
