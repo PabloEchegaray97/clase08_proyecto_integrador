3c8b1c5dc4c158cd42f20330629d34c69157afd6
pae <pablo.echegaray97@gmail.com>
2023-08-07 02:09:07 -0300

    cart-router methods

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -1,19 +1,20 @@
 import { Router } from 'express';
 import CartModel from '../DAO/mongoManager/models/cart.model.js';
+import cartModel from '../DAO/mongoManager/models/cart.model.js';
 
 const router = Router();
 
 router.get('/', async (req, res) => {
     try {
         const carts = await CartModel.find().lean().exec();
-        res.render('carts', {carts})
+        res.render('carts', { carts })
     } catch (error) {
         console.error('Error fetching carts from the database:', error);
         res.status(500).send('Error fetching carts from the database');
     }
 });
 
-router.get('/:cartId/:productId', async (req, res) => {
+router.get('/:cartId/products/:productId', async (req, res) => {
     try {
         const cartId = req.params.cartId;
         const productId = req.params.productId;
@@ -38,6 +39,82 @@ router.get('/:cartId/:productId', async (req, res) => {
     }
 });
 
+router.delete('/:cartId/products/:productId', async (req, res) => {
+    try {
+        const cartId = req.params.cartId;
+        const productId = req.params.productId;
+        let cart = await CartModel.findById(cartId);
+        const productIndex = cart.products.findIndex(
+            (product) => product.id == productId
+        );
+        if (productIndex !== -1) {
+            cart.products.splice(productIndex, 1);
+            cart = await cart.save()
+            res.status(200).send(cart);
+        } else {
+            res.status(200).send('Not modified')
+        }
+    } catch (error) {
+        console.error('Error deleting product:', error);
+        res.status(500).send('Error deleting product')
+    }
+})
+router.delete('/:cartId', async (req, res) => {
+    try {
+        const cartId = req.params.cartId;
+        let cart = await CartModel.findById(cartId);
+        cart.products = [];
+        cart = await cart.save();
+        res.status(200).send(cart);
+    } catch (error) {
+        console.error('Error deleting products from cart');
+        res.status(500).send('Error deleting products from cart ')
+    }
+})
+router.put('/:cartId', async (req, res) => {
+    try {
+        const cartId = req.params.cartId;
+        const updatedCart = req.body;
+        let cart = await cartModel.findById(cartId);
+        if (!cart) {
+            return res.status(404).send('Cart not found');
+        }
+        cart.products = updatedCart.products;
+        cart = await cart.save();
+        res.status(200).json(cart);
+    } catch (error) {
+        console.error('Error updating cart:', error)
+        res.status(500).send('Error updating cart')
+    }
+})
+
+router.put('/:cartId/products/:productId', async (req, res) => {
+    try {
+        const cartId = req.params.cartId;
+        const productId = req.params.productId;
+        const { quantity } = req.body;
+        let cart = await CartModel.findById(cartId);
+        if (!cart) {
+            return res.status(404).send('Cart not found');
+        }
+        const productIndex = cart.products.findIndex(
+            (product) => product.id === productId
+        );
+        if (productIndex !== -1) {
+            cart.products[productIndex].quantity = quantity;
+            cart = await cart.save();
+            res.status(200).json(cart);
+        } else {
+            res.status(404).send('Product not found')
+        }
+    } catch (error) {
+        console.error('Error updating product quantity:', error);
+        res.status(500).send('Error updating product quantity');
+    }
+})
+router.delete('/:cartId', (req, res) => {
+
+})
 router.post('/', async (req, res) => {
     try {
         const cart = await CartModel.create({ products: [] });
