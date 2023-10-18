9fb6714d2f01b038fca839982c343c321ce7f33e
pae <pablo.echegaray97@gmail.com>
2023-10-18 04:49:56 -0300

    errors.log with date & time

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -21,19 +21,23 @@ const isProd = process.env.APP_ENV === 'development'
     
 export const logger = winston.createLogger({
     levels: customLevelOptions.levels,
+    format: winston.format.combine(
+        winston.format.simple(),
+        winston.format.printf(info => {
+            const formatted_date = new Date().toLocaleString()
+            return `${formatted_date} [${info.level.toUpperCase()}] - ${info.message}`;
+        })
+    ),
     transports: [
         new winston.transports.Console({
-            level: isProd ? 'debug' : 'info',
-            format: winston.format.combine(winston.format.simple())
+            level: isProd ? 'debug' : 'info'
         }),
         new winston.transports.File({
             filename: './errors.log',
             level: 'warning',
-            format: winston.format.simple()
         })
     ]
 })
-
 
 export const addLogger = (req, res, next) => {
     req.logger = logger
